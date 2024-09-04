c3e84a7f8276e5966bfb7a825811aa15bd0b07e9
sheldonelias <schiffer@oxy.edu>
2024-09-04 13:10:40 -0700

    update

---
+++ src/_17_Interfacess_SpanishSpeaker.java
@@ -0,0 +1,26 @@
+public class _17_Interfacess_SpanishSpeaker implements _17_Interfaces
+{
+    @Override
+    public void sayGreeting()
+    {
+        System.out.println("Hola.");
+    }
+
+    @Override
+    public void sayParting()
+    {
+        System.out.println("Adios.");
+    }
+
+    @Override
+    public void sayStop()
+    {
+        System.out.println("Alto.");
+    }
+
+    @Override
+    public void sayGo() {
+
+        System.out.println("Anda.");
+    }
+}
